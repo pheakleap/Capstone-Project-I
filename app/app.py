f94f459b7349558cdf1797c627f2d27c156cbdee
zinhour10 <zinhour10@gmail.com>
2025-02-19 22:18:00 +0700

    first hand on gradio

---
+++ app/app.py
@@ -0,0 +1,12 @@
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text"],
+    outputs=["text"],
+)
+
+demo.launch()
